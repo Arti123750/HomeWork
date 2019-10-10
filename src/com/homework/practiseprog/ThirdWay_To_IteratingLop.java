f5f3f4fd163e142136b066f027e89c1ca80f2812
Arti123750 <bombalearti@gmail.com>
2019-10-10 14:11:37 +0530

    OneMore Prog Is Added

---
+++ src/com/homework/practiseprog/ThirdWay_To_IteratingLop.java
@@ -0,0 +1,23 @@
+package com.homework.practiseprog;
+
+import java.util.ArrayList;
+
+public class ThirdWay_To_IteratingLop {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ArrayList<String>a1=new ArrayList<>();
+		a1.add("Arti");
+		a1.add("Pooja");
+		a1.add("Ashwini");
+		//a1.add("Sakshi");
+		//a1.add("Namrata");
+		
+		System.out.println(" :Iterate loop Using ForEach Method: ");
+		a1.forEach(a->System.out.println(a1));
+
+	}
+
+}
